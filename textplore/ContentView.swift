a9680f2a5c06e45405372c282d4c9b0d864e8efb
Haryanto Quang <quang.haryanto@gmail.com>
2021-08-05 11:40:26 +0700

    Initial Commit

---
+++ textplore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  textplore
+//
+//  Created by Haryanto Salim on 05/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
